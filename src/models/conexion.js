a77a380fd64fdbaccb1e7e3ce5b26932ea7348f1
JoseGarPe <jhosuegarciastarkand@gmail.com>
2020-12-20 23:18:19 -0600

    Update conexion.js

--- src/models/conexion.js
+++ src/models/conexion.js
@@ -1,8 +1,9 @@
 const mysql = require('mysql');
+//mysql://be9cd74a1aad6b:63cb72bf@us-cdbr-east-02.cleardb.com/heroku_c5963e5ac37fee1?reconnect=true
 const connection = mysql.createConnection({
-    host: 'localhost',
-    user:'root',
-    password:'',
-    database:'camera_data_capture'
+    host: 'us-cdbr-east-02.cleardb.com',
+    user:'be9cd74a1aad6b',
+    password:'63cb72bf',
+    database:'heroku_c5963e5ac37fee1'
 });
 module.exports = connection;
